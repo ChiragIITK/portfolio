9937350aa0ef33b1d419a26220155bea8d952130
Chirag <chirag.kumawat@credgenics.com>
2025-04-30 21:28:53 +0530

    removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
-
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center 
