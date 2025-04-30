f7d3907dcc23300dee8d108695be7f3aa4192c85
Chirag <chirag.kumawat@credgenics.com>
2025-04-30 21:28:28 +0530

    removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
-import Travel1 from "@/components/Travel1";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Slideshow from "@/components/ui/SlideShow";
-import ThreeColumnSlideshow from "@/components/ui/ThreeColumnSlideshow";
 import { navItems } from "@/data";
 
 import Image from "next/image";
